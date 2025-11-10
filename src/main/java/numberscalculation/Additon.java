e5de37ca1612fc95afd5c74f9fe5b631f2119700
prafulla-timilsina <radha1timilsina@gmail>
2025-11-10 17:33:29 +0545

    print the multiplication in the addtion file

--- src/main/java/numberscalculation/Additon.java
+++ src/main/java/numberscalculation/Additon.java
@@ -6,6 +6,8 @@ public class Additon {
 
     public static void main(String[] args) {
         int sum = num1+num2;
+        int mult = num1*num2;
         System.out.println("The sum of two number is:"+sum);
+        System.out.println("The multiplication of the two number is:"+mult);
     }
 }
