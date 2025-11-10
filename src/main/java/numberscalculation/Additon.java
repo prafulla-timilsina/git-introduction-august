2645f526d6622b40211b3c1fbfa6bc38604070f6
prafulla-timilsina <radha1timilsina@gmail>
2025-11-10 17:27:50 +0545

    addtion is added

---
+++ src/main/java/numberscalculation/Additon.java
@@ -0,0 +1,11 @@
+package numberscalculation;
+
+public class Additon {
+    static int num1 = 5;
+    static int num2 = 10;
+
+    public static void main(String[] args) {
+        int sum = num1+num2;
+        System.out.println("The sum of two number is:"+sum);
+    }
+}
