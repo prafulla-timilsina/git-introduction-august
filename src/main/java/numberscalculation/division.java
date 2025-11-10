c1add9c70623dd938c39b1de57fa1509d51efbc6
prafulla-timilsina <radha1timilsina@gmail>
2025-11-10 17:39:10 +0545

    the division of the two numbers is added

---
+++ src/main/java/numberscalculation/division.java
@@ -0,0 +1,11 @@
+package numberscalculation;
+
+public class division {
+    public static void main(String[] args) {
+        int num1 = 1000;
+        int num2 = 10;
+        int division = num1/num2;
+
+        System.out.println("The division of two number is:"+division);
+    }
+}
